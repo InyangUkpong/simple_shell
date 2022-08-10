c22b085d37fa8ad2e0e805c925219b8f62d34fae
InyangUkpong <inyangweb@gmail.com>
2022-08-10 16:57:20 -0700

    fixed betty error

--- alias_builtins.c
+++ alias_builtins.c
@@ -4,7 +4,7 @@
  */
 #include "shell.h"
 
-int shellby_alias(char **args, char __attribute__((__unused__)) * *front);
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
 void set_alias(char *var_name, char *value);
 void print_alias(alias_t *alias);
 
@@ -17,7 +17,7 @@ void print_alias(alias_t *alias);
  * Return: If an error occurs - -1.
  *         Otherwise - 0.
  */
-int shellby_alias(char **args, char __attribute__((__unused__)) * *front)
+int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 {
 	alias_t *temp = aliases;
 	int i, ret = 0;
